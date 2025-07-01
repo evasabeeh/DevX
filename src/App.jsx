30a9d54bc6fefda034ac23be9199ef7d28ab5cad
Eva Sabeeh <evas190401@gmail.com>
2025-07-02 04:27:05 +0530

    text aligned

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import Polygon3 from "./components/Polygon3";
 
 const App = () => {
   return (
-    <div className="min-h-screen px-4 py-16 text-center mr-10 md:mr-5">
-      <h1 className="text-3xl font-bold text-darkergreen mb-2">“Voices of Success”</h1>
+    <div className="min-h-screen px-4 py-16 text-center">
+      <h1 className="text-4xl font-bold text-darkergreen mb-2">“Voices of Success”</h1>
       <p className="text-darkergreen font-bold mb-20 text-2xl">Our Student Stories</p>
 
-      <div className="flex flex-col md:flex-row md:justify-center md:items-start gap-10">
+      <div className="flex flex-col md:flex-row md:justify-center md:items-start gap-10 mr-5 md:mr-10">
         <Polygon1 />
         <Polygon2 />
         <Polygon3 />
